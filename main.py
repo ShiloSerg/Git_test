934e8e247d0ca0f20f571ed79e6c75bee12b4254
Sergey Shilo <Parol24051989!>
2022-05-24 22:34:22 +0300

    task 7 and main changes

--- main.py
+++ main.py
@@ -8,30 +8,30 @@ from tasks import task_7
 from tasks import task_8
 import time
 
-#
-# # task_1
-#
-# assert task_1.bytes_to_dec([0, 1, 1, 0]) == 6
-# assert task_1.bytes_to_dec([1, 1, 1, 1]) == 15
-#
-#
-# # task_2
-# task_2.wave('hello')
-#
-# # task_3
-# for elem in task_3.endless_wave('hello'):
-#     print(elem)
-#
-# # task_4
-# task_4.get_strong_word('Какое то слово сильнее')
-#
-# # task_5
-# task_5.turn_out('male')
-# task_5.turn_out('mouse')
-#
-# # task_6
-# assert task_6.roman_number('XXI') == 21
-# assert task_6.roman_number('MCMXC') == 1990
+
+# task_1
+
+assert task_1.bytes_to_dec([0, 1, 1, 0]) == 6
+assert task_1.bytes_to_dec([1, 1, 1, 1]) == 15
+
+
+# task_2
+task_2.wave('hello')
+
+# task_3
+for elem in task_3.endless_wave('hello'):
+    print(elem)
+
+# task_4
+task_4.get_strong_word('Какое то слово сильнее')
+
+# task_5
+task_5.turn_out('male')
+task_5.turn_out('mouse')
+
+# task_6
+assert task_6.roman_number('XXI') == 21
+assert task_6.roman_number('MCMXC') == 1990
 
 #
 task_7.generate_person()
