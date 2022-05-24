6bdfbde80f51d15e3a3ff02e867e0f0c44a712bd
Sergey Shilo <Parol24051989!>
2022-05-24 21:21:15 +0300

    task 7 done

---
+++ tasks/task_7.py
@@ -0,0 +1,35 @@
+from faker import Faker
+from datetime import datetime
+import json
+import os
+
+fake = Faker('ru_RU')
+
+
+def generate_person():
+    new_person = [{'name': fake.first_name_male(),
+                   'surname': fake.last_name_male(),
+                   'login': fake.user_name(),
+                   'password': fake.password(),
+                   'address': fake.address(),
+                   'email': fake.email(),
+                   'phone': fake.phone_number(),
+                   'register_time': datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}]
+
+    file_name = 'test.json'
+    if os.stat(file_name).st_size == 0:
+        with open(file_name, 'w', encoding="utf8") as f:
+            json.dump(new_person, f, indent=4, ensure_ascii=False)
+    else:
+        with open(file_name, 'r', encoding="utf8") as f:
+            data = json.load(f)
+            print(data)
+            # data = list(data)
+            data += new_person
+        with open(file_name, 'w', encoding="utf8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+            # print(json.dumps(new_person, indent=4))
+
+
+if __name__ == '__main__':
+    generate_person()
