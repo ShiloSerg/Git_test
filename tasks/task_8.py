70eb7655df42b43ca91aef1231a7553099d82b5d
Sergey Shilo <Parol24051989!>
2022-05-24 21:14:21 +0300

    task 6 and 8 done

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 
 def user_create(unix_time: float, user_format: str) -> str:
+    user_format = user_format.lower()
     rep1 = user_format.replace('dd', datetime.utcfromtimestamp(unix_time).strftime('%d'))
     rep2 = rep1.replace('mm', datetime.utcfromtimestamp(unix_time).strftime('%m'))
     rep3 = rep2.replace('yyyy', datetime.utcfromtimestamp(unix_time).strftime('%Y'))
