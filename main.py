58d407b788575189279bef31654fc451e8f8a796
Sergey Shilo <Parol24051989!>
2022-05-24 22:29:34 +0300

    task 4, 7, main changes

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from tasks import task_1, task_2
+from tasks import task_1
+from tasks import task_2
+from tasks import task_3
+from tasks import task_4
+from tasks import task_5
+from tasks import task_6
+from tasks import task_7
+from tasks import task_8
+import time
 
-# task_1
+#
+# # task_1
+#
+# assert task_1.bytes_to_dec([0, 1, 1, 0]) == 6
+# assert task_1.bytes_to_dec([1, 1, 1, 1]) == 15
+#
+#
+# # task_2
+# task_2.wave('hello')
+#
+# # task_3
+# for elem in task_3.endless_wave('hello'):
+#     print(elem)
+#
+# # task_4
+# task_4.get_strong_word('Какое то слово сильнее')
+#
+# # task_5
+# task_5.turn_out('male')
+# task_5.turn_out('mouse')
+#
+# # task_6
+# assert task_6.roman_number('XXI') == 21
+# assert task_6.roman_number('MCMXC') == 1990
 
-assert task_1.bytes_to_dec([0, 1, 1, 0]) == 6
-assert task_1.bytes_to_dec([1, 1, 1, 1]) == 15
+#
+task_7.generate_person()
 
-
-# task_2
-task_2.wave('hello')
-
-# task_3
+# task_8
+task_8.user_create(time.time(), "yyyy-mm-dd")
--- tasks/task_4.py
+++ tasks/task_4.py
@@ -1,15 +1,15 @@
-def get_strong_word(str_: str) -> str:
+def get_strong_word(str_: str) -> int:
     data = str_.split()
+    list_strong_words = []
     for word in data:
         word_strong = 0
         for ch in word:
             if ch.isalpha():
                 word_strong += ord(ch)
-        print(word_strong)
+        list_strong_words.append(word_strong)
+    print(max(list_strong_words))
+    return max(list_strong_words)
 
 
-get_strong_word('Какое то слово сильнее')
-
-
-
-
+if __name__ == '__main__':
+    get_strong_word('Какое то слово сильнее')
